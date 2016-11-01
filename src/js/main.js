31a1e9bd9a4b47504bf334e53fe7c8d2daaab795
Serge Kovbasiuk <kolbasovs@gmail.com>
2016-11-01 18:01:47 +0200

    init

--- src/js/main.js
+++ src/js/main.js
@@ -1,72 +1,189 @@
-$(document).ready(function() {
-    // var burger = $('.burger'),
-    //     mainMenu = $('.main-menu');
+// burger & menu
+const burger = document.querySelector('.header__burger');
+const close = document.querySelector('.header__close');
+const menu = document.getElementById('menu');
+const navItems = document.querySelectorAll('.nav__item');
 
-    $('#fullpage').fullpage({
-      menu: '#menu',
-      anchors:['aboutPage', 'worksPage', 'processPage', 'pricingPage', 'contactsPage'],
-      css3: true,
-    });
+const handleMenu = () => {
+  if (menu.classList.contains('is-open')) {
+    menu.classList.remove('is-open');
+  } else {
+    menu.classList.add('is-open');
+  }
+};
 
-    // burger.on('click', function () {
-    //   mainMenu.addClass('is-open');
-    //   burger.hide();
-    // });
-    //
-    // $('.close-icon, .main-menu__link').on('click', function () {
-    //   mainMenu.removeClass('is-open');
-    //   burger.show();
-    // });
-    //
-    // $('.arrow-up').on('click', function (e) {
-    //   e.preventDefault();
-    //   $.fn.fullpage.moveSectionUp();
-    // });
-    //
-    $('.scroll-down').on('click', function (e) {
-      e.preventDefault();
-      $.fn.fullpage.moveSectionDown();
-    });
+burger.addEventListener('click', (e) => {
+  e.preventDefault();
+  handleMenu();
+});
 
-    $('.works__items').slick({
-      dots: true,
-      customPaging : function(slider, i) {
-        return '<button type="button" data-role="none" role="button" tabindex="0">0' + (i + 1) + '.</button>';
-      },
-      nextArrow: '<button type="button" class="slick-next">next</button>',
-      prevArrow: '<button type="button" class="slick-prev">prev</button>',
-      infinite: true,
-      slidesToShow: 3,
-      slidesToScroll: 1
-    });
+close.addEventListener('click', (e) => {
+  e.preventDefault();
+  handleMenu();
+});
 
-    // tabs
-    const links = $('.pricing__link');
-    const pricingItem = $('.pricing__item');
+for (let i = 0, max = navItems.length; i < max; i += 1) {
+  navItems[i].addEventListener('click', () => {
+    handleMenu();
+  });
+}
 
-    links.on('click', function(e) {
-      if ($(e.target).is($('#domesticLink'))) {
-        links.removeClass('is-active');
-        $(this).addClass('is-active');
-        pricingItem.removeClass('is-active');
-        $('#domestic').addClass('is-active');
-      } else if ($(e.target).is($('#internationalLink'))) {
-        links.removeClass('is-active');
-        $(this).addClass('is-active');
-        pricingItem.removeClass('is-active');
-        $('#international').addClass('is-active');
-      } else if ($(e.target).is($('#lookbookLink'))) {
-        links.removeClass('is-active');
-        $(this).addClass('is-active');
-        pricingItem.removeClass('is-active');
-        $('#lookbook').addClass('is-active');
-      } else {
-        links.removeClass('is-active');
-        $(this).addClass('is-active');
-        pricingItem.removeClass('is-active');
-        $('#smm').addClass('is-active');
+// tabs
+(() => {
+  const tabs = (options) => {
+    const el = document.querySelector(options.el);
+    const tabLinks = document.querySelectorAll(options.tabLinks);
+    const tabContainers = document.querySelectorAll(options.tabContainers);
+    let activeTab = 0;
+    let initialized = false;
+
+    const goToTab = (index) => {
+      if (index !== activeTab && index >= 0 && index <= tabLinks.length) {
+        tabLinks[activeTab].classList.remove('is-active');
+        tabLinks[index].classList.add('is-active');
+        tabContainers[activeTab].classList.remove('is-active');
+        tabContainers[index].classList.add('is-active');
+        activeTab = index;
       }
-    });
+    };
+
+    const handleClick = (link, index) => {
+      link.addEventListener('click', (e) => {
+        e.preventDefault();
+        goToTab(index);
+      });
+    };
+
+    const init = () => {
+      if (!initialized) {
+        initialized = true;
+
+        for (let i = 0; i < tabLinks.length; i += 1) {
+          const link = tabLinks[i];
+          handleClick(link, i);
+        }
+      }
+    };
+
+    return {
+      init,
+      goToTab,
+    };
+  };
+
+  window.tabs = tabs;
+})();
+
+const contactTabs = tabs({
+  el: '.contacts',
+  tabLinks: '.contacts__tab',
+  tabContainers: '.contacts__tab-container',
+});
+
+contactTabs.init();
+
+// portfolio modal
+const portfolioButton = document.querySelector('.button--projects');
+const portfolioModal = document.querySelector('.portfolio');
+const closeModal = document.querySelector('.header__close--portfolio');
+
+portfolioButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  portfolioModal.classList.add('is-open');
+  $.fn.fullpage.setAllowScrolling(false);
+  $.fn.fullpage.setKeyboardScrolling(false);
+});
+
+closeModal.addEventListener('click', (e) => {
+  e.preventDefault();
+  portfolioModal.classList.remove('is-open');
+  $.fn.fullpage.setAllowScrolling(true);
+  $.fn.fullpage.setKeyboardScrolling(true);
+});
 
 
+// jQuery plugins
+$(document).ready(() => {
+  // fullpage
+  $('#fullpage').fullpage({
+    menu: '#menu',
+    anchors: ['aboutPage', 'worksPage', 'processPage', 'pricingPage', 'contactsPage'],
+    css3: true,
+  });
+
+  $('.scroll-down').on('click', (e) => {
+    e.preventDefault();
+    $.fn.fullpage.moveSectionDown();
+  });
+
+  // slick
+  $('.works__items').slick({
+    dots: true,
+    customPaging: (slider, i) => (
+      `<button type="button" data-role="none" role="button" tabindex="0">0${i + 1}.</button>`
+    ),
+    nextArrow: '<button type="button" class="slick-next">next</button>',
+    prevArrow: '<button type="button" class="slick-prev">prev</button>',
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [{
+      breakpoint: 426,
+      settings: {
+        slidesToShow: 1,
+        variableWidth: true,
+      },
+    }, {
+      breakpoint: 300,
+      settings: 'unslick',
+    }],
+  });
+
+  // tabs
+  const links = $('.pricing__link');
+  const tabs = $('.pricing__tabs');
+  const pricingItem = $('.pricing__item');
+  const back = $('.item__back');
+
+  (() => {
+    if (window.innerWidth <= 769) {
+      pricingItem.first().removeClass('is-active');
+      links.first().removeClass('is-active');
+    }
+  })();
+
+  back.on('click', (e) => {
+    e.preventDefault();
+    tabs.removeClass('is-hidden');
+    pricingItem.removeClass('is-active');
+  });
+
+  links.on('click', (e) => {
+    e.preventDefault();
+    if ($(e.target).is($('#domesticLink'))) {
+      links.removeClass('is-active');
+      $('#domesticLink').addClass('is-active');
+      pricingItem.removeClass('is-active');
+      $('#domestic').addClass('is-active');
+      tabs.addClass('is-hidden');
+    } else if ($(e.target).is($('#internationalLink'))) {
+      links.removeClass('is-active');
+      $('#internationalLink').addClass('is-active');
+      pricingItem.removeClass('is-active');
+      $('#international').addClass('is-active');
+      tabs.addClass('is-hidden');
+    } else if ($(e.target).is($('#lookbookLink'))) {
+      links.removeClass('is-active');
+      $('#lookbookLink').addClass('is-active');
+      pricingItem.removeClass('is-active');
+      $('#lookbook').addClass('is-active');
+      tabs.addClass('is-hidden');
+    } else {
+      links.removeClass('is-active');
+      $('#smmLink').addClass('is-active');
+      pricingItem.removeClass('is-active');
+      $('#smm').addClass('is-active');
+      tabs.addClass('is-hidden');
+    }
+  });
 });
