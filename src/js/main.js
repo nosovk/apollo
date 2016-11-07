4efa47bfa4157ec516a3c00806b254e30c181a5d
Serge Kovbasiuk <kolbasovs@gmail.com>
2016-11-07 19:32:40 +0200

    fullpage disable on mobile

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,128 @@
+// portfolio imgs
+// filter
+// const limit = 20; // number of images to show
+const imgCategories = document.querySelectorAll('.portfolio__category');
+const optionAll = imgCategories[0];
+const optionPhoto = imgCategories[1];
+const optionVideo = imgCategories[2];
+const optionLookbook = imgCategories[3];
+const itemsAll = [].slice.call(document.querySelectorAll('.portfolio__item'));
+const itemsPhoto = [].slice.call(document.querySelectorAll('.item--photo'));
+const itemsVideo = [].slice.call(document.querySelectorAll('.item--video'));
+const itemsLookbook = [].slice.call(document.querySelectorAll('.item--lookbook'));
+
+// const isActive = element => element.classList.contains('is-active');
+// const isInvisible = element => !element.classList.contains('is-visible');
+// const makeVisible = (imgCategory) => {
+//   if (imgCategory.length > limit) {
+//     for (let n = 0; n < limit; n += 1) {
+//       imgCategory[n].classList.add('is-visible');
+//     }
+//   } else {
+//     for (let n = 0; n < imgCategory.length; n += 1) {
+//       imgCategory[n].classList.add('is-visible');
+//     }
+//   }
+// };
+// const lazyLoadImages = () => {
+//   // loads only imgs with class 'is-visible'
+//   const images = document.querySelectorAll('.is-visible img[data-src]');
+//
+//   for (let i = 0; i < images.length; i += 1) {
+//     images[i].setAttribute('src', images[i].getAttribute('data-src'));
+//     images[i].removeAttribute('data-src');
+//   }
+// };
+//
+// const isInViewport = (element) => {
+//   const rect = element.getBoundingClientRect();
+//
+//   return (
+//     rect.top >= 0 &&
+//     rect.left >= 0 &&
+//     rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+//     rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+//   );
+// };
+//
+// const loadHandler = () => {
+//   const images = document.querySelectorAll('img[data-src]');
+//
+//   [].forEach.call(images, (item) => {
+//     if (isInViewport(item)) {
+//       item.parentElement.classList.add('is-visible');
+//     }
+//     lazyLoadImages();
+//   });
+// };
+//
+// document.querySelector('.portfolio__inner').addEventListener('scroll', loadHandler);
+// window.addEventListener('scroll', loadHandler);
+// window.addEventListener('DOMContentLoaded', loadHandler);
+// window.addEventListener('load', loadHandler);
+// window.addEventListener('resize', loadHandler);
+
+const myLazyLoad = new LazyLoad({
+  container: document.querySelector('.portfolio__items'),
+  threshold: 500,
+  throttle: 30,
+});
+
+const hide = (items) => {
+  for (let i = 0; i < items.length; i += 1) {
+    items[i].style.display = 'none';
+  }
+};
+const show = (items) => {
+  for (let i = 0; i < items.length; i += 1) {
+    items[i].style.display = 'block';
+  }
+};
+
+for (let i = 0; i < imgCategories.length; i += 1) {
+  imgCategories[i].addEventListener('click', (e) => {
+    e.preventDefault();
+    optionAll.classList.remove('is-active');
+    optionPhoto.classList.remove('is-active');
+    optionVideo.classList.remove('is-active');
+    optionLookbook.classList.remove('is-active');
+
+    // hide all items
+
+
+    if (e.target === optionAll) {
+      console.log('all clicked');
+      optionAll.classList.add('is-active');
+      hide(itemsAll);
+      show(itemsAll);
+    } else if (e.target === optionPhoto) {
+      console.log('photo clicked');
+      optionPhoto.classList.add('is-active');
+      hide(itemsAll);
+      show(itemsPhoto);
+    } else if (e.target === optionVideo) {
+      console.log('video clicked');
+      optionVideo.classList.add('is-active');
+      hide(itemsAll);
+      show(itemsVideo);
+    } else {
+      console.log('lookbook clicked');
+      optionLookbook.classList.add('is-active');
+      hide(itemsAll);
+      show(itemsLookbook);
+    }
+
+    // loadHandler();
+  });
+}
+
+// // initial loading
+// (() => {
+//
+//   lazyLoadImages();
+// })();
+
+
 // burger & menu
 const burger = document.querySelector('.header__burger');
 const close = document.querySelector('.header__close');
@@ -156,6 +281,7 @@ const closeModal = document.querySelector('.header__close--portfolio');
 portfolioButton.addEventListener('click', (e) => {
   e.preventDefault();
   portfolioModal.classList.add('is-open');
+  // loadHandler();
   // disable body scroll
   $.fn.fullpage.setAllowScrolling(false);
   $.fn.fullpage.setKeyboardScrolling(false);
@@ -169,6 +295,40 @@ closeModal.addEventListener('click', (e) => {
   $.fn.fullpage.setKeyboardScrolling(true);
 });
 
+const btnToContacts = document.querySelector('.button--to-contacts');
+
+const animate = (elem, style, unit, from, to, time, prop) => {
+  if (!elem) {
+    return;
+  }
+  const start = new Date().getTime(),
+    timer = setInterval(() => {
+      const step = Math.min(1, (new Date().getTime() - start) / time);
+      if (prop) {
+        elem[style] = (from + (step * (to - from))) + unit;
+      } else {
+        elem.style[style] = (from + (step * (to - from))) + unit;
+      }
+      if (step === 1) {
+        clearInterval(timer);
+      }
+    }, 25);
+  elem.style[style] = from + unit;
+};
+
+btnToContacts.addEventListener('click', () => {
+  const target = document.getElementById('contacts');
+  animate(document.body, 'scrollTop', '', 0, target.offsetTop, 400, true);
+});
+
+const pricingLinks = document.querySelectorAll('.pricing__link');
+
+for (let i = 0; i < pricingLinks.length; i += 1) {
+  pricingLinks[i].addEventListener('click', () => {
+    const target = document.getElementById('pricing');
+    animate(document.body, 'scrollTop', '', window.pageYOffset, target.offsetTop, 400, true);
+  });
+}
 
 // jQuery plugins
 $(document).ready(() => {
@@ -178,6 +338,10 @@ $(document).ready(() => {
     anchors: ['aboutPage', 'worksPage', 'processPage', 'pricingPage', 'contactsPage'],
     css3: true,
   });
+
+  if (window.innerWidth <= 425) {
+    $.fn.fullpage.destroy('all');
+  }
 
   $('.scroll-down').on('click', (e) => {
     e.preventDefault();
